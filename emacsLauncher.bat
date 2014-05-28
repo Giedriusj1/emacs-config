394d04946c20544dcd6116ebeb2f5443fd0b55b5
Giedriusj1 <giedriusj1@gmail.com>
2014-05-28 09:15:18 +0100

    .bat changes

--- emacsLauncher.bat
+++ emacsLauncher.bat
@@ -4,5 +4,5 @@ set HOME=C:\Users\Giedrius\Desktop\emacs-24.3\bin
 set PATH=%HOME%\UnxUpdates;%PATH%
 ::set HOME=%CD%\..\
 
-bitsadmin /transfer mydownloadjob  /download /priority normal https://raw.githubusercontent.com/Giedriusj1/emacs-config/master/emacs.org %HOME%/.emacs.d/emacs.org
+bitsadmin /transfer getEmacsConfig  /download /priority normal https://raw.githubusercontent.com/Giedriusj1/emacs-config/master/emacs.org %HOME%/.emacs.d/emacs.org
 "%~dp0emacsclientw.exe" -na  "%~dp0runemacs.exe " "%1"
