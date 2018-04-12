ec266417edb837b8956644d3a10f9d5c347fa095
Giedrius Jonikas <giedriusj1@gmail.com>
2018-04-12 10:31:40 +0100

    1. Better font selection on Windows. 2. Dropping SSL usage for marmalade repo, because it makes Windows machines sad.

--- emacsLauncher.bat
+++ emacsLauncher.bat
@@ -1,8 +1,2 @@
-@echo off
-::set PATH=C:\Users\Giedrius\Desktop\UnxUpdates;%PATH%
-set HOME=C:\Users\Giedrius\Desktop\emacs-24.3\bin
-set PATH=%HOME%\UnxUpdates;%PATH%
-::set HOME=%CD%\..\
-
-bitsadmin /transfer getEmacsConfig  /download /priority normal https://raw.githubusercontent.com/Giedriusj1/emacs-config/master/emacs.org %HOME%/.emacs.d/emacs.org
-"%~dp0emacsclientw.exe" -na  "%~dp0runemacs.exe " "%1"
+set HOME=%cd%\..\
+..\emacs.exe
