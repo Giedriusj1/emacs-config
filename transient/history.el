d4a4d4cbfbf8de4dcba8725ebaae3690c4b620b5
Giedrius Jonikas <g.jonikas@resilientplc.com>
2019-05-13 12:37:17 +0100

    treat .pm files as perl

---
+++ transient/history.el
@@ -0,0 +1,11 @@
+((magit-blame
+  ("-w"))
+ (magit-commit nil)
+ (magit-diff
+  ("--diff-algorithm=patience" "--no-ext-diff" "--stat"))
+ (magit-diff:--diff-algorithm)
+ (magit-dispatch nil)
+ (magit-ediff nil)
+ (magit-fetch nil)
+ (magit-pull nil)
+ (magit-stash nil))
