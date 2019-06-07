80ee4fa6bb9609e45fd24a96c600a3c6b9ad5755
Giedrius Jonikas <g.jonikas@resilientplc.com>
2019-06-07 16:41:01 +0100

    early start

---
+++ early-init.el
@@ -0,0 +1,13 @@
+(setq package-quickstart t)
+
+;; (setq tool-bar-mode nil
+;;       menu-bar-mode nil
+;;       scroll-bar-mode nil)
+
+
+(customize-set-variable 'scroll-bar-mode nil)
+(customize-set-variable 'horizontal-scroll-bar-mode nil)
+(customize-set-variable 'menu-bar-mode nil)
+(customize-set-variable 'tool-bar-mode nil)
+
+(message "from very early")
