293f588cac5c31bc37a64866f184435257bc3436
Giedrius <giedriusj1@gmail.com>
2021-11-29 16:07:35 +0000

    comment out blog stuff

--- config/blog.el
+++ config/blog.el
@@ -1,19 +1,19 @@
-(use-package htmlize :defer t)
-;; default one would pick source colours from my current theme...
-(setq org-html-htmlize-output-type 'css)
+;; (use-package htmlize :defer t)
+;; ;; default one would pick source colours from my current theme...
+;; (setq org-html-htmlize-output-type 'css)
 
-(setq org-publish-project-alist
-      '(("org-blog"
-	     :base-directory "~/private-sync/blog/"
-	     :base-extension "org"
-	     :publishing-directory "~/public_html/"
-	     :recursive t
-	     :publishing-function org-html-publish-to-html
-	     :headline-levels 4             ; Just the default for this project.
-	     :auto-preamble t )
-	    ("org-static"
-	     :base-directory "~/private-sync/blog/"
-	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-	     :publishing-directory "~/public_html/"
-	     :recursive t
-	     :publishing-function org-publish-attachment)))
+;; (setq org-publish-project-alist
+;;       '(("org-blog"
+;; 	     :base-directory "~/private-sync/blog/"
+;; 	     :base-extension "org"
+;; 	     :publishing-directory "~/public_html/"
+;; 	     :recursive t
+;; 	     :publishing-function org-html-publish-to-html
+;; 	     :headline-levels 4             ; Just the default for this project.
+;; 	     :auto-preamble t )
+;; 	    ("org-static"
+;; 	     :base-directory "~/private-sync/blog/"
+;; 	     :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+;; 	     :publishing-directory "~/public_html/"
+;; 	     :recursive t
+;; 	     :publishing-function org-publish-attachment)))
