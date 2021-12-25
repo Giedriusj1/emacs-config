38a7af1e83cd30f01936f91b86906069fc106e15
Giedrius <giedriusj1@gmail.com>
2021-12-25 19:06:28 +0000

    playground will now reload helm-tree-sitter too

--- config/playground.el
+++ config/playground.el
@@ -3,13 +3,16 @@
 (defun load-listp-playground (args)
   "docstring"
   (interactive "P")
+  ;; Load playground
   (dolist (element (seq-filter (lambda (item)
                                  (and (string-match-p ".el$" item)
                                       (not (member item basic-load-sequence))))
                                (directory-files "~/private-sync/projects/lisp-playground")))
-    (load (concat "~/private-sync/projects/lisp-playground/" element))))
+    (load (concat "~/private-sync/projects/lisp-playground/" element)))
 
-
-
-
-
+  ;; And then reload helm-tree-sitter
+  (dolist (element (seq-filter (lambda (item)
+                                 (and (string-match-p ".el$" item)
+                                      (not (member item basic-load-sequence))))
+                               (directory-files "~/.emacs.d/config/helm-tree-sitter")))
+    (load (concat "~/.emacs.d/config/helm-tree-sitter/" element))))
