d699b55df01d1c6eca8d24df0a74a2b894526d59
Giedrius <giedriusj1@gmail.com>
2022-01-04 23:26:28 +0000

    helm-tree-sitter on demand

--- config/helm.el
+++ config/helm.el
@@ -2,8 +2,8 @@
   :bind
   (("C-j" . helm-mini))
   (:map control-semi-map
-        (( "C-s" . helm-tree-sitter-or-imenu)
-         ( "s" . helm-tree-sitter-debug)
+        (( "C-s" . g/control-semi-map-helm-tree-sitter-or-imenu)
+         ( "s" .   g/control-semi-map-helm-tree-sitter-debug)
          ( "l" . helm-M-x)
          ( "r" . helm-mark-ring)
          ( "C-r" . helm-global-mark-ring)
@@ -12,6 +12,19 @@
 
   (:map lisp-interaction-mode-map (("C-j" . helm-mini)))
   :config
+
+  (defun g/control-semi-map-helm-tree-sitter-debug ()
+    (interactive)
+    (require 'helm-tree-sitter-debug)
+    (tree-sitter-mode)
+    (helm-tree-sitter-debug))
+
+  (defun g/control-semi-map-helm-tree-sitter-or-imenu ()
+    (interactive)
+    (require 'helm-tree-sitter)
+    (tree-sitter-mode)
+    (helm-tree-sitter-or-imenu))
+  
   (setq helm-candidate-number-limit 500)
   (setq helm-buffer-max-length 60)
 
@@ -22,7 +35,6 @@
               (("C-m" . helm-swoop)
                ("m" . helm-multi-swoop-all))))
 
-
 (use-package swiper-helm :defer 20
   :bind (:map control-semi-map (())
               ("o" . swiper-helm)
@@ -32,23 +44,28 @@
 
 (use-package helm-rg :defer t)
 
-;; This effectively disables idle reparsing for all files
-(setq semantic-idle-scheduler-max-buffer-size 1)
-
-;; We don't care about saving db when exiting emacs
-(remove-hook 'kill-emacs-hook #'semanticdb-kill-emacs-hook)
-
-(defun ds () t)
-(add-hook 'semantic-inhibit-functions  #'ds)
-
-(defun g/helm-semantic-or-imenu (arg)
-  (interactive "P")
-  (remove-hook 'semantic-inhibit-functions #'ds)
-  (semantic-new-buffer-fcn)
-  (helm-semantic-or-imenu arg)
-  (add-hook 'semantic-inhibit-functions  #'ds))
-
-
 (use-package view :defer t :pin manual
   :bind (:map view-mode-map
               ("C-j" . helm-mini)))
+
+;; (use-package semantic :defer t :pin manual
+  ;; :init
+  ;; (defun g/helm-semantic-or-imenu (arg)
+  ;;   (interactive "P")
+  ;;   (remove-hook 'semantic-inhibit-functions #'ds)
+  ;;   (semantic-new-buffer-fcn)
+  ;;   (helm-semantic-or-imenu arg)
+  ;;   (add-hook 'semantic-inhibit-functions  #'ds))
+  
+  ;; :config
+  ;; ;; This effectively disables idle reparsing for all files
+  ;; (setq semantic-idle-scheduler-max-buffer-size 1)
+
+  ;; ;; We don't care about saving db when exiting emacs
+  ;; (remove-hook 'kill-emacs-hook #'semanticdb-kill-emacs-hook)
+
+  ;; (defun ds () t)
+  ;; (add-hook 'semantic-inhibit-functions  #'ds)
+  ;; )
+
+
--- config/tree-sitter.el
+++ config/tree-sitter.el
@@ -2,15 +2,3 @@
 (use-package tree-sitter :defer t)
 
 (use-package tree-sitter-langs :defer t)
-
-(require 'helm-tree-sitter)
-(require 'helm-tree-sitter-debug)
-
-(global-tree-sitter-mode)
-
-(define-key lisp-playground-map (kbd "SPC") 'load-listp-playground)
-(define-key lisp-playground-map (kbd "g") 'g-tree)
-(define-key lisp-playground-map (kbd "s") 'helm-tree-sitter-or-imenu)
-
-;TODO: We want to use-package with sensible autoloads, etc...
-;; (require 'helm-tree-sitter)
