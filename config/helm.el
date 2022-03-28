fe9389fca93b7243a758bf0fa7cdab0a1359f0ff
Giedrius <giedriusj1@gmail.com>
2022-03-28 14:57:19 +0100

    Clean up config/helm.el

--- config/helm.el
+++ config/helm.el
@@ -26,7 +26,7 @@
     (require 'helm-tree-sitter)
     (tree-sitter-mode)
     (helm-tree-sitter-or-imenu))
-  
+
   (setq helm-candidate-number-limit 500)
   (setq helm-buffer-max-length 60)
 
@@ -49,25 +49,3 @@
 (use-package view :defer t :pin manual
   :bind (:map view-mode-map
               ("C-j" . helm-mini)))
-
-;; (use-package semantic :defer t :pin manual
-  ;; :init
-  ;; (defun g/helm-semantic-or-imenu (arg)
-  ;;   (interactive "P")
-  ;;   (remove-hook 'semantic-inhibit-functions #'ds)
-  ;;   (semantic-new-buffer-fcn)
-  ;;   (helm-semantic-or-imenu arg)
-  ;;   (add-hook 'semantic-inhibit-functions  #'ds))
-  
-  ;; :config
-  ;; ;; This effectively disables idle reparsing for all files
-  ;; (setq semantic-idle-scheduler-max-buffer-size 1)
-
-  ;; ;; We don't care about saving db when exiting emacs
-  ;; (remove-hook 'kill-emacs-hook #'semanticdb-kill-emacs-hook)
-
-  ;; (defun ds () t)
-  ;; (add-hook 'semantic-inhibit-functions  #'ds)
-  ;; )
-
-
