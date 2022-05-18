618df01b3c0fe04bbfe72f77e77c4fa956063458
Giedrius Jonikas <g.jonikas@resilientplc.com>
2022-05-18 14:19:37 +0100

    use tab-bar-mode instead of eyebrowse

---
+++ config/tab-bar.el
@@ -0,0 +1,26 @@
+;;; -*- lexical-binding: t -*-
+
+(tab-bar-mode)
+
+(global-set-key (kbd "<C-tab>") 'tab-bar-switch-to-next-tab)
+(global-set-key (kbd "<C-iso-lefttab>") 'tab-bar-switch-to-prev-tab)
+(global-set-key (kbd "<C-S-tab>") 'tab-bar-switch-to-prev-tab)
+
+(defhydra hydra-frame-helper
+  (:color blue)
+  "
+ tab-bar                 frame management
+------------------------------------------
+                        [_M-m_]ake frame
+[_M-c_]reate              [_M-o_]ther frame
+[_M-k_]lose current       [_M-SPC_]other frame
+                        [_M-d_]elete frame
+"
+  ("M-m" make-frame nil)
+  ("M-o" other-frame nil)
+  ("M-SPC" other-frame nil)
+  ("M-d" delete-frame nil)
+  ("M-c" tab-bar-new-tab)
+  ("M-k" tab-bar-close-tab))
+
+(global-set-key (kbd "M-SPC") 'hydra-frame-helper/body)
