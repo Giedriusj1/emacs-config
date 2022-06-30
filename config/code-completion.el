1ccf3332b0e7b9191c9be8c3d093342f135efd59
Giedrius <giedriusj1@gmail.com>
2022-06-30 18:46:41 +0100

    Use company-quickhelp instead of company-box

--- config/code-completion.el
+++ config/code-completion.el
@@ -8,16 +8,7 @@
   :hook ((prog-mode . yas-minor-mode)
          (org-mode . yas-minor-mode)))
 
-(when (display-graphic-p)
-  (use-package  company-box :defer t
-    :hook (company-mode . company-box-mode)
-    :config
-    (setq company-box-backends-colors
-          '((company-lsp .
-                         ( :selected (:background "orange"  :foreground "black")))
-            (company-capf .
-			              ( :selected (:background "orange" :foreground "black")))))
-    (setq company-box-doc-delay 1)))
+(use-package company-quickhelp :defer t)
 
 (use-package company :defer t
   :bind ( :map company-active-map
@@ -28,17 +19,9 @@
            ("C-n" . dabbrev-expand)))
   :diminish company-mode
   :config
-  ;; (when (display-graphic-p)
-  ;;   (require 'color)
-
-  ;;   (let ((bg (face-attribute 'default :background)))
-  ;;     (custom-set-faces
-  ;;      `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-  ;;      `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-  ;;      `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-  ;;      `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-  ;;      `(company-tooltip-common ((t (:inherit font-lock-constant-face)))))))
-
   (global-company-mode t)
+  (company-quickhelp-mode)
+
+  (setq pos-tip-background-color "#121717")
 
   (setq company-tooltip-limit 25))
