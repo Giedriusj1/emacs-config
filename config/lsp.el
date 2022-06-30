18c58c2120e8d38af12478a8e7078cccd9af2fef
Giedrius <giedriusj1@gmail.com>
2022-06-30 23:50:58 +0100

    lsp improvements

--- config/lsp.el
+++ config/lsp.el
@@ -1,7 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
 (on-linux
- (use-package lsp-ui :defer t)
  (use-package helm-lsp :defer t)
 
  (use-package pretty-hydra :ensure t :defer t)
@@ -13,11 +12,11 @@
                   (-first (-lambda ((mode-or-pattern . language))
                             (eq mode-or-pattern major-mode)))
                   cl-rest)
-        (and
-         ;; and lsp-mode is not enabled
-         (not (bound-and-true-p lsp-mode))
-         ;; and we are not on Windows
-         (not (is-windows))))
+             (and
+              ;; and lsp-mode is not enabled
+              (not (bound-and-true-p lsp-mode))
+              ;; and we are not on Windows
+              (not (is-windows))))
 
         ;; Enable LSP
         (progn (message "enabling lsp mode...") (lsp)))
@@ -25,18 +24,16 @@
     :foreign-keys warn :title "LSP" :quit-key "q" :color blue)
    ("Buffer"
     (("f" lsp-format-buffer "format")
-     ("menu" lsp-ui-imenu "imenu")
-     ("c" ace-delete-other-windows "maximize")
-     ("u" helm-lsp-code-actions "execute action"))
+     ("u" helm-lsp-code-actions "execute action")
+     ("h" helm-lsp-diagnostics "diagnostics")
+     )
     "Server"
-    (("M-r" move-border-left "restart")
-     ("S" lsp-shutdown-workspace "restart")
-     ("M-s" lsp-describe-session "shutdown"))
+    (("S" lsp-restart-workspace "restart"))
     "Symbol"
     (("d" lsp-find-declaration "declaration")
-     ("D" lsp-ui-peek-find-definitions "definition")
-     ("R" lsp-ui-peek-find-references "references")
-     ("i" lsp-ui-peek-find-implementation "implementation")
+     ("D" lsp-find-definition "definition")
+     ("R" lsp-find-references "references")
+     ("i" lsp-find-implementation "implementation")
      ("o" lsp-describe-thing-at-point "documentation")
      ("t" lsp-find-type-definition "type")
      ("r" lsp-rename "rename")
