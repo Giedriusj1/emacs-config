511f392e46e27b313bb3902ed8612c106f68061e
Giedrius <giedriusj1@gmail.com>
2022-08-12 16:34:58 +0100

    private sync only on linux

--- config/private-sync.el
+++ config/private-sync.el
@@ -1,20 +1,20 @@
 ;;; -*- lexical-binding: t -*-
+(on-linux
+ (i-defun private-sync-fn ()
+   (let* ((default-directory "~/private-sync"))
+     (g/message "pulling private-sync repo")
+     (start-process "proc-git-pull" "notes-sync-output" "git" "pull")))
 
-(i-defun private-sync-fn ()
-  (let* ((default-directory "~/private-sync"))
-	(g/message "pulling private-sync repo")
-	(start-process "proc-git-pull" "notes-sync-output" "git" "pull")))
+ ;; Run the above every 2 mins (if we are idle)
+ (run-with-idle-timer (* 60 2) t 'private-sync-fn)
 
-;; Run the above every 2 mins (if we are idle)
-(run-with-idle-timer (* 60 2) t 'private-sync-fn)
+ (use-package git-auto-commit-mode :defer t
+   :diminish git-auto-commit-mode)
 
-(use-package git-auto-commit-mode :defer t
-  :diminish git-auto-commit-mode)
+ (defun turn-on-auto-commit-hook ()
+   (cond ((string-match (concat "^" (expand-file-name "~/private-sync")) buffer-file-name)
+          (progn
+            (git-auto-commit-mode 1)
+            (setq gac-automatically-push-p t)))))
 
-(defun turn-on-auto-commit-hook ()
-  (cond ((string-match (concat "^" (expand-file-name "~/private-sync")) buffer-file-name)
-         (progn
-           (git-auto-commit-mode 1)
-           (setq gac-automatically-push-p t)))))
-
-(add-hook 'find-file-hook 'turn-on-auto-commit-hook)
+ (add-hook 'find-file-hook 'turn-on-auto-commit-hook))
