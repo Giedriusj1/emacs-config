e73af31da9212a1b9bb2b981f92a90227371d44f
Giedrius <giedriusj1@gmail.com>
2022-08-12 15:53:16 +0100

    remove unneeded function

--- config/behaviour.el
+++ config/behaviour.el
@@ -93,14 +93,6 @@
     (pixel-scroll-precision-mode)
   (pixel-scroll-mode))
 
-(defun my-create-non-existent-directory ()
-  (let ((parent-directory (file-name-directory buffer-file-name)))
-    (when (and (not (file-exists-p parent-directory))
-               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
-      (make-directory parent-directory t))))
-
-(add-to-list 'find-file-not-found-functions #'my-create-non-existent-directory)
-
 (define-key tab-map (kbd "j")
             (i-lambda () (cond ((eq 'org-mode major-mode)
 	                            (hydra-org/body))
