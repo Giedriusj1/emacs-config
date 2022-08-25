82573b92e1c0006e29effbca4cd5b7ba14d149b2
Giedrius <giedriusj1@gmail.com>
2022-08-25 23:56:13 +0100

    fix SWIFT

--- config/swift.el
+++ config/swift.el
@@ -1,46 +1,43 @@
 ;;; -*- lexical-binding: t -*-
 
-(i-defun swift-up(&optional arg)
-  (or arg (setq arg 1))
-  (dotimes (bind arg)
-    (scroll-down-line)
-    (previous-line)))
-
-(i-defun swift-down(&optional arg)
-  (or arg (setq arg 1))
-  (dotimes (bind arg)
-    (scroll-up-line)
-    (next-line)))
-
 (define-key control-semi-map (kbd "C-f") 'global-swift-mode)
 
-(defvar swift-command-map
-  (let ((map (make-sparse-keymap)))
-    ;; movement
-    (define-key map (kbd "i") (i-lambda () (swift-up 2)))
-
-    (define-key map (kbd "k") (i-lambda () (swift-down 2)))
-
-    (define-key map (kbd "o") 'swift-up)
-    (define-key map (kbd "l") 'swift-down)
-
-    (define-key map (kbd "p") 'beginning-of-defun)
-    (define-key map (kbd "n") 'end-of-defun)
-
-    (define-key map (kbd "u") 'cua-scroll-down)
-
-    (define-key map (kbd "j") 'cua-scroll-up)
-
-    ;; cua mode
-    (define-key map (kbd "C-z") 'toggle-swift-mode)
-    (define-key map (kbd "C-x") 'kill-region)
-    (define-key map (kbd "C-c") 'kill-ring-save)
-    (define-key map (kbd "C-v") 'yank)
-    map))
-
 (define-minor-mode swift-mode
+  "Move around swiftly"
   :lighter " === SWIFT ==="
-  :keymap swift-command-map
+  :keymap (let ((map (make-sparse-keymap)))
+	    ;; movement
+	    (define-key map (kbd "i") (i-lambda () (dotimes (bind 2)
+						     (scroll-down-line)
+						     (previous-line))))
+
+	    (define-key map (kbd "k") (i-lambda () (dotimes (bind 2)
+						     (scroll-up-line)
+						     (next-line))))
+
+	    (define-key map (kbd "o") (i-lambda () (progn
+						     (scroll-down-line)
+						     (previous-line))))
+	    
+	    (define-key map (kbd "l") (i-lambda () (progn
+						     (scroll-up-line)
+						     (next-line))))
+
+	    (define-key map (kbd "p") 'beginning-of-defun)
+	    (define-key map (kbd "n") 'end-of-defun)
+
+	    (define-key map (kbd "u") 'cua-scroll-down)
+
+	    (define-key map (kbd "j") 'cua-scroll-up)
+
+	    ;; cua mode
+	    (define-key map (kbd "C-z") 'toggle-swift-mode)
+	    (define-key map (kbd "C-x") 'kill-region)
+	    (define-key map (kbd "C-c") 'kill-ring-save)
+	    (define-key map (kbd "C-v") 'yank)
+	    map)
+
+  
   (if (eq global-swift-mode t)
       (custom-set-faces '(cursor ((t (:background "blue")))))
     (custom-set-faces '(cursor ((t (:background "OrangeRed")))))))
