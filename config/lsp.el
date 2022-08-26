2a0aac3eb87496b483d4b6c598be9195f5ab56f7
Giedrius <giedriusj1@gmail.com>
2022-08-26 10:01:26 +0100

    nicer lsp when dealing with elisp

--- config/lsp.el
+++ config/lsp.el
@@ -33,31 +33,22 @@
 		    ("s" lsp-signature-help "signature"))))
 
    :config
-   (setq lsp-diagnostic-package :none)
-   (i-defun describe-foo-at-point ()
-     "Show the documentation of the Elisp function and variable near point.
-	This checks in turn:
-	-- for a function name where point is
-	-- for a variable name where point is
-	-- for a surrounding function call
-	"
-     (let (sym)
-       ;; sigh, function-at-point is too clever.  we want only the first half.
-       (cond ((setq sym (ignore-errors
-                          (with-syntax-table emacs-lisp-mode-syntax-table
-                            (save-excursion
-                              (or (not (zerop (skip-syntax-backward "_w")))
-                                  (eq (char-syntax (char-after (point))) ?w)
-                                  (eq (char-syntax (char-after (point))) ?_)
-                                  (forward-sexp -1))
-                              (skip-chars-forward "`'")
-        	              (let ((obj (read (current-buffer))))
-				(and (symbolp obj) (fboundp obj) obj))))))
-              (describe-function sym))
-             ((setq sym (variable-at-point)) (describe-variable sym))
-             ;; now let it operate fully -- i.e. also check the
-             ;; surrounding sexp for a function call.
-             ((setq sym (function-at-point)) (describe-function sym)))))
+   (i-defun describe-thing-in-popup ()
+     (require 'popup)
+     (require 'xref)
+
+     (let* ((thing (symbol-at-point))
+            (help-xref-following t)
+            (description (with-temp-buffer
+                           (help-mode)
+                           (help-xref-interned thing)
+                           (buffer-string))))
+       (popup-tip description
+		  :point (point)
+		  :around t
+		  :height 300
+		  :scroll-bar t
+		  :margin t)))
 
    (i-defun g-lsp/invoke ()
      (require 'lsp-mode)
@@ -75,7 +66,7 @@
 
    (pretty-hydra-define hydra-describe (:foreign-keys warn  :quit-key "q" :color blue)
      ("Symbol"
-      (("d" describe-foo-at-point "declaration")
+      (("d" describe-thing-in-popup "declaration")
        ("D" find-function-at-point "find function"))
       "Eval"
       (("b" eval-buffer "buffer")
