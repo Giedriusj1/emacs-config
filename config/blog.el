addc23b8cd2dd53818e69db4db33708268adaf37
Giedrius <giedriusj1@gmail.com>
2022-08-29 21:30:19 +0100

    refactor blog a litle bit

--- config/blog.el
+++ config/blog.el
@@ -2,27 +2,27 @@
 
 (on-linux
  (i-defun g/org-publish-all-force ()
-   (progn
-     (require 'htmlize)
+   (progn (use-package htmlize)
+	  (require 'htmlize)
 
-     (require 'ox-publish)
-     ;; default one would pick source colours from my current theme...
-     (setq org-html-htmlize-output-type 'css)
+	  (require 'ox-publish)
+	  ;; default one would pick source colours from my current theme...
+	  (setq org-html-htmlize-output-type 'css)
 
-     (setq org-publish-project-alist
-	   '(("org-blog"
-	      :base-directory "~/private-sync/blog/"
-	      :base-extension "org"
-	      :publishing-directory "~/public_html/"
-	      :recursive t
-	      :publishing-function org-html-publish-to-html
-	      :headline-levels 4             ; Just the default for this project.
-	      :auto-preamble t )
-	     ("org-static"
-	      :base-directory "~/private-sync/blog/"
-	      :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-	      :publishing-directory "~/public_html/"
-	      :recursive t
-	      :publishing-function org-publish-attachment)))
+	  (setq org-publish-project-alist
+		'(("org-blog"
+		   :base-directory "~/private-sync/blog/"
+		   :base-extension "org"
+		   :publishing-directory "~/public_html/"
+		   :recursive t
+		   :publishing-function org-html-publish-to-html
+		   :headline-levels 4             ; Just the default for this project.
+		   :auto-preamble nil )
+		  ("org-static"
+		   :base-directory "~/private-sync/blog/"
+		   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+		   :publishing-directory "~/public_html/"
+		   :recursive t
+		   :publishing-function org-publish-attachment)))
 
-     (org-publish-all t))))
+	  (org-publish-all t))))
