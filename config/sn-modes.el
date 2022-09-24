7890147efab2742cb74ebad9ab808d83c9b4225d
Giedrius <giedriusj1@gmail.com>
2022-09-24 19:22:21 +0100

    move adl and opensips modes into sn-modes.el

--- config/sn-modes.el
+++ config/sn-modes.el
@@ -392,3 +392,15 @@
         (" on \\|goto " . font-lock-keyword-face)))))
 
 (add-to-list 'auto-mode-alist '("\\.adl\\'" . adl-mode))
+
+
+(define-derived-mode opensips-cfg-mode c-mode "opensips-cfg"
+  "major mode for editing opensips cfg code."
+  (setq font-lock-defaults '(("#.*" . font-lock-comment-face)        
+			     ;; ("\ .*(" . font-lock-function-name-face)
+			     ("record_route\\|socket\\|server_signature\\|tcp_workers\\|udp_workers\\|log_facility\\|route\\|is_method\\|modparam\\|xlog_level\\|loadmodule\\|mpath\\|debug_mode\\|log_level\\|xlog\\|log_stderror\\|auto_aliases"
+			      . font-lock-constant-face)
+			     ("on \\| if\\|goto\\|exit" . font-lock-keyword-face))))
+
+(add-to-list 'auto-mode-alist '("\\opensips.*.cfg\\'" . opensips-cfg-mode))
+
