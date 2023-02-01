ccbad5ce873fd4d7d04688db56fa22e81fcf4bbc
Giedrius <giedriusj1@gmail.com>
2023-02-01 22:26:22 +0000

    Better eglot enabling logic

--- config/lsp.el
+++ config/lsp.el
@@ -7,11 +7,12 @@
    ;; enable by default on high memory machines
    (when (> (string-to-number (shell-command-to-string "free -m | awk '/^Mem/ {print $2}'")) 32000)
      (add-hook 'python-mode-hook 'eglot-ensure)
+     (add-hook 'python-ts-mode-hook 'eglot-ensure)
      (add-hook 'c++-mode-hook 'eglot-ensure)
      (add-hook 'c-mode-hook 'eglot-ensure)
      (add-hook 'typescript-mode-hook 'eglot-ensure)
      (add-hook 'typescript-ts-mode-hook 'eglot-ensure)
-     (add-hook 'rustic-mode-hook 'eglot-ensure))
+     (add-hook 'rust-ts-mode-hook 'eglot-ensure))
    :config
    (transient-define-prefix g/eglot-transient ()
      ["Buffer"
