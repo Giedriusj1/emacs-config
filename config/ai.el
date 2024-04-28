c6bf002b2fe801bbad0e635a0dec94321a74b19d
Giedrius <giedriusj1@gmail.com>
2024-04-28 21:28:33 +0100

    comment out gpt.el and some unneeded deps

--- config/ai.el
+++ config/ai.el
@@ -1,9 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
 (on-linux
- (g/up s)
- (g/up editorconfig)
-
  ;; git clone https://github.com/zerolfx/copilot.el ~/prog/EMACS/copilot.el
  (let ((f "~/prog/EMACS/copilot.el/copilot.el"))
    (when (file-exists-p f)
@@ -25,34 +22,35 @@
        )))
 
 
- (g/up gpt :bind ("C--" . g/gpt-transient)
-   :config
-   (transient-define-prefix g/gpt-transient ()
-     ["codex"
-      ("c" "codex temp 0" (lambda ()
-                            (interactive)
-                            (setq gpt-openai-temperature "0")
-                            (setq gpt-openai-model "code-davinci-003")
-                            (setq gpt-openai-max-tokens "2000")
-                            (gpt-dwim)))
-      ("C" "codex temp 0.5" (lambda ()
-                              (interactive)
-                              (setq gpt-openai-temperature "0.5")
-                              (setq gpt-openai-model "code-davinci-003")
-                              (setq gpt-openai-max-tokens "2000")
-                              (gpt-dwim)))]
-     ["GPT-3"
-      ("d" "Davinci temp 0" (lambda ()
-                              (interactive)
-                              (setq gpt-openai-temperature "0")
-                              (setq gpt-openai-model "gpt-3.5-turbo")
-                              (message "gpt-openai-model: %s" gpt-openai-model)
-                              (setq gpt-openai-max-tokens "2000")
-                              (gpt-dwim)))
-      ("D" "Davinci temp 0.5" (lambda ()
-                                (interactive)
-                                (setq gpt-openai-temperature "0.5")
-                                (setq gpt-openai-model "gpt-3.5-turbo")
-                                (setq gpt-openai-max-tokens "4000")
-                                (gpt-dwim)))
-      ])))
+ ;; (g/up gpt :bind ("C--" . g/gpt-transient)
+ ;;   :config
+ ;;   (transient-define-prefix g/gpt-transient ()
+ ;;     ["codex"
+ ;;      ("c" "codex temp 0" (lambda ()
+ ;;                            (interactive)
+ ;;                            (setq gpt-openai-temperature "0")
+ ;;                            (setq gpt-openai-model "code-davinci-003")
+ ;;                            (setq gpt-openai-max-tokens "2000")
+ ;;                            (gpt-dwim)))
+ ;;      ("C" "codex temp 0.5" (lambda ()
+ ;;                              (interactive)
+ ;;                              (setq gpt-openai-temperature "0.5")
+ ;;                              (setq gpt-openai-model "code-davinci-003")
+ ;;                              (setq gpt-openai-max-tokens "2000")
+ ;;                              (gpt-dwim)))]
+ ;;     ["GPT-3"
+ ;;      ("d" "Davinci temp 0" (lambda ()
+ ;;                              (interactive)
+ ;;                              (setq gpt-openai-temperature "0")
+ ;;                              (setq gpt-openai-model "gpt-3.5-turbo")
+ ;;                              (message "gpt-openai-model: %s" gpt-openai-model)
+ ;;                              (setq gpt-openai-max-tokens "2000")
+ ;;                              (gpt-dwim)))
+ ;;      ("D" "Davinci temp 0.5" (lambda ()
+ ;;                                (interactive)
+ ;;                                (setq gpt-openai-temperature "0.5")
+ ;;                                (setq gpt-openai-model "gpt-3.5-turbo")
+ ;;                                (setq gpt-openai-max-tokens "4000")
+ ;;                                (gpt-dwim)))
+ ;;      ]))
+ )
