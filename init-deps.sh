8241f74e269282b454046df543c9c7d295c2d82e
Giedrius <giedriusj1@gmail.com>
2024-08-13 21:50:22 +0100

    use depth 1 when checking out deps

--- init-deps.sh
+++ init-deps.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-git clone git@github.com:Giedriusj1/emacs-elpa-deps.git elpa --depth 1
+git clone --depth=1 git@github.com:Giedriusj1/emacs-elpa-deps.git elpa
 
-git clone https://github.com/zerolfx/copilot.el
+git clone --depth=1  https://github.com/zerolfx/copilot.el
