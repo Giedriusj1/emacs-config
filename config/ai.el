60833f12bae109298e0ce35fb82b1f2708c2539f
Giedrius Jonikas <giedriusj1@gmail.com>
2025-02-15 12:35:58 +0000

    copilot.el is not in melpa :)

--- config/ai.el
+++ config/ai.el
@@ -1,32 +1,25 @@
 ;;; -*- lexical-binding: t -*-
 
 (on-linux
- (when (file-exists-p "~/.emacs.d/copilot.el/copilot.el")
-   (progn
-     (add-to-list 'load-path (expand-file-name "~/.emacs.d/copilot.el"))
+ (g/up copilot
+   :init
+   (dolist (mode '(prog-mode-hook text-mode-hook))
+     (add-hook
+      mode
+      (lambda ()
+        (require 'copilot)
 
-     (g/up s)
-     (g/up f)
-     (g/up dash)
-     (g/up editorconfig)
+        (if (copilot-installed-version)
+            (progn
 
-     (dolist (mode '(prog-mode-hook text-mode-hook))
-       (add-hook
-        mode
-        (lambda ()
-          (require 'copilot)
+              (i-defun copilot-accept-or-indent-for-tab-command (&optional arg)
+                (if (copilot--overlay-visible)
+                    (copilot-accept-completion)
+                  (indent-for-tab-command arg)))
 
-          (if (copilot-installed-version)
-              (progn
-
-                (i-defun copilot-accept-or-indent-for-tab-command (&optional arg)
-                  (if (copilot--overlay-visible)
-                      (copilot-accept-completion)
-                    (indent-for-tab-command arg)))
-
-                (global-set-key (kbd "TAB") 'copilot-accept-or-indent-for-tab-command)
+              (global-set-key (kbd "TAB") 'copilot-accept-or-indent-for-tab-command)
 
 
-                (copilot-mode))
+              (copilot-mode))
 
-            (message "Copilot server is not installed"))))))))
+          (message "Copilot server is not installed")))))))
