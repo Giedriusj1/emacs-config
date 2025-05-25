a0032273b72cdb602e46d25dd0187933c20b9b67
Giedrius Jonikas <giedriusj1@gmail.com>
2025-05-25 14:04:37 +0100

    do not defer loading copilot.el

--- config/ai.el
+++ config/ai.el
@@ -1,16 +1,15 @@
 ;;; -*- lexical-binding: t -*-
 
 (on-linux
- (g/up copilot
+ (g/up copilot :defer nil
    :init
 
    (defalias 'cop 'copilot-mode)
 
    (dolist (mode '(prog-mode-hook text-mode-hook))
-     (add-hook
-      mode
-      (lambda ()
-        (bind-keys* ( "C-M-i" . copilot-accept-completion))
+     (add-hook mode
+               (lambda ()
+                 (bind-keys* ( "C-M-i" . copilot-accept-completion))
 
-        (copilot-mode)
-        )))))
+                 (copilot-mode)
+                 )))))
