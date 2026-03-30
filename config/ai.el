e3163681306ccfad5654b0f719d52f3855d06aa1
Giedrius Jonikas <giedriusj1@gmail.com>
2026-03-30 17:19:45 +0200

    comment out claude code

--- config/ai.el
+++ config/ai.el
@@ -20,34 +20,34 @@
 
                  ))))
 
- ;; CLAUDE CODE
- ;; install required inheritenv dependency:
- (use-package inheritenv
-   :vc (:url "https://github.com/purcell/inheritenv" :rev :newest))
+ ;; ;; CLAUDE CODE
+ ;; ;; install required inheritenv dependency:
+ ;; (use-package inheritenv
+ ;;   :vc (:url "https://github.com/purcell/inheritenv" :rev :newest))
 
- ;; for eat terminal backend:
- (use-package eat :ensure t)
+ ;; ;; for eat terminal backend:
+ ;; (use-package eat :ensure t)
 
- ;; for vterm terminal backend:
- (use-package vterm :ensure t)
+ ;; ;; for vterm terminal backend:
+ ;; (use-package vterm :ensure t)
 
- ;; install claude-code.el
- (use-package claude-code :ensure t
-   :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
-   :config
+ ;; ;; install claude-code.el
+ ;; (use-package claude-code :ensure t
+ ;;   :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+ ;;   :config
 
-   (use-package monet
-     :vc (:url "https://github.com/stevemolitor/monet" :rev :newest))
+ ;;   (use-package monet
+ ;;     :vc (:url "https://github.com/stevemolitor/monet" :rev :newest))
 
-   ;; optional IDE integration with Monet
-   (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
-   (monet-mode 1)
+ ;;   ;; optional IDE integration with Monet
+ ;;   (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
+ ;;   (monet-mode 1)
 
-   (claude-code-mode)
-   :bind-keymap ("C-c c" . claude-code-command-map)
+ ;;   (claude-code-mode)
+ ;;   :bind-keymap ("C-c c" . claude-code-command-map)
 
-   ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
-   :bind
-   (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
+ ;;   ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+ ;;   :bind
+ ;;   (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode)))
 
  )
