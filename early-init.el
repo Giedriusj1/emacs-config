d5c435c31c21c0c652b806df37a7f7230d4b694c
Giedrius Jonikas <giedriusj1@gmail.com>
2026-05-28 21:23:11 +0100

    move where we init graphics

--- early-init.el
+++ early-init.el
@@ -73,24 +73,6 @@
 
 (customize-set-variable 'custom-enabled-themes '(wombat))
 
-(when (display-graphic-p)
-  (custom-set-faces
-   '(org-level-1 ((t (:inherit outline-1 :weight demibold :height 1.2))))
-   '(default ((t (:background "#131818"))))
-   '(cursor-orig ((t (:inherit cursor))))
-   '(mode-line-inactive ((t (:background "#101010"))))
-   '(mode-line ((t (:background "#404040"))))
-   '(hl-line ((t (:inherit nil :background "#222222"))))
-   '(minibuffer-prompt ((t (:foreground "#ff584d"))))
-
-   ;; Make some default wombat colours a bit more lively
-   '(font-lock-builtin-face ((((class color) (min-colors 89)) (:foreground "#ff685d"))))
-   '(font-lock-constant-face ((((class color) (min-colors 89)) (:foreground "#ff685d"))))
-
-   ;; Reduce red in agent-shell
-   '(error ((t (:foreground "#ff9f43"))))
-   '(warning ((t (:foreground "#feca57"))))))
-
 (global-hl-line-mode t)
 (fringe-mode 0)
 (tooltip-mode -1)
--- init.el
+++ init.el
@@ -3,6 +3,24 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
+(when (display-graphic-p)
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :weight demibold :height 1.2))))
+   '(default ((t (:background "#131818"))))
+   '(cursor-orig ((t (:inherit cursor))))
+   '(mode-line-inactive ((t (:background "#101010"))))
+   '(mode-line ((t (:background "#404040"))))
+   '(hl-line ((t (:inherit nil :background "#222222"))))
+   '(minibuffer-prompt ((t (:foreground "#ff584d"))))
+
+   ;; Make some default wombat colours a bit more lively
+   '(font-lock-builtin-face ((((class color) (min-colors 89)) (:foreground "#ff685d"))))
+   '(font-lock-constant-face ((((class color) (min-colors 89)) (:foreground "#ff685d"))))
+
+   ;; Reduce red in agent-shell
+   '(error ((t (:foreground "#ff9f43"))))
+   '(warning ((t (:foreground "#feca57"))))))
+
 ;; On Windows system we'll just ignore signatures altogether...
 ;; too much hassle.
 (on-windows (setq package-check-signature nil)
